9f05e24e63c0f1d5e9249834b5c22fb9a789925a
cassieopeanuts <92317800+cassieopeanuts@users.noreply.github.com>
2023-12-15 14:43:49 +0900

    init

---
+++ testDeposit.js
@@ -0,0 +1,67 @@
+const Web3 = require('web3');
+const fs = require('fs');
+
+// Load ABIs
+const yourContractABI = JSON.parse(fs.readFileSync('your_contract_abi.json', 'utf-8'));
+const tokenContractABI = JSON.parse(fs.readFileSync('token_contract_abi.json', 'utf-8'));
+
+// Contract addresses
+const yourContractAddress = '0xsomething'; // Your contract address
+const tokenContractAddress = '0xsomething'; // Token contract address you approving to spend
+
+// Your Web3 provider
+const moonriverRpcUrl = 'wss://wss.api.moonriver.moonbeam.network'; // Moonriver's public RPC as example
+const web3 = new Web3(moonriverRpcUrl);
+
+// Wallet setup
+const privateKey = 'your tester address private key'; // Private key of the address from which transaction comes from
+const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+web3.eth.accounts.wallet.add(account);
+
+// Contract instances
+const yourContract = new web3.eth.Contract(yourContractABI, yourContractAddress);
+const tokenContract = new web3.eth.Contract(tokenContractABI, tokenContractAddress);
+
+// Approve your contract to spend tokens
+async function approveTokens() {
+    try {
+     
+        const approvalAmount = web3.utils.toWei('10', 'Tokens'); // Example amount is set for 10 tokens
+        const estimatedGas = await yourContract.methods.deposit(approvalAmount).estimateGas({ from: account.address });
+        console.log('Estimated Gas:', estimatedGas);
+        const gasLimit = Math.floor(estimatedGas * 1.2);
+   
+        const approvalResult = await tokenContract.methods.approve(yourContractAddress, approvalAmount).send({ 
+            from: account.address,
+            gasLimit: gasLimit
+        });
+        console.log('Token approval result:', approvalResult);
+    } catch (error) {
+        console.error('Error in token approval:', error);
+    }
+}
+
+// Deposit approved tokens into your contract
+async function testDeposit() {
+    try {
+        await approveTokens(); 
+
+        const depositAmount = web3.utils.toWei('10', 'Tokens');
+        const estimatedGas = await yourContract.methods.deposit(depositAmount).estimateGas({ from: account.address });
+        console.log('Estimated Gas:', estimatedGas);
+        const gasLimit = Math.floor(estimatedGas * 1.2);
+
+        const depositResult = await yourContract.methods.deposit(depositAmount).send({
+            from: account.address,
+            gasLimit: gasLimit
+        });
+
+        console.log('Deposit result:', depositResult);
+    } catch (error) {
+        console.error('Error in deposit:', error);
+    }
+}
+
+testDeposit();
+
+// run with npm start
